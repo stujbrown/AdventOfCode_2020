9f50275f6b7b5e60de9ad99957b7f5fb643d02ab
stuart.j.brown.90@gmail.com <stuart.j.brown.90@gmail.com>
2020-12-06 13:42:13 +0000

    Day 6 part 1

---
+++ Day6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day6
+{
+    using CharacterSet = HashSet<char>;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CharacterSet> answerGroups = new List<CharacterSet>();
+            CharacterSet currentGroup = new CharacterSet();
+            using (TextReader reader = File.OpenText("input.txt"))
+            {
+                Action recordGroup = () =>
+                {
+                    answerGroups.Add(currentGroup);
+                    currentGroup = new CharacterSet();
+                };
+
+                while (reader.Peek() >= 0)
+                {
+                    string line = reader.ReadLine();
+                    if (String.IsNullOrWhiteSpace(line))
+                    {
+                        recordGroup();
+                    }
+                    else
+                    {
+                        foreach (char character in line)
+                        {
+                            currentGroup.Add(character);
+                        }
+                    }
+                }
+
+                recordGroup();
+
+                int sum = 0;
+                foreach (CharacterSet group in answerGroups)
+                {
+                    sum += group.Count;
+                }
+                Console.WriteLine("Sum: {0}", sum );
+            }
+        }
+    }
+}

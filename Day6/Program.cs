8669f0bfef96eb2b5802201712ff8393a6b10236
stuart.j.brown.90@gmail.com <stuart.j.brown.90@gmail.com>
2020-12-06 15:23:01 +0000

    Day 6 part 2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day6
 {
-    using CharacterSet = HashSet<char>;
+    using CharacterCountDictionary = Dictionary<char, int>;
 
     class Program
     {
+        struct Group
+        {
+            public CharacterCountDictionary Answers;
+            public int NumPeople;
+        };
+
         static void Main(string[] args)
         {
-            List<CharacterSet> answerGroups = new List<CharacterSet>();
-            CharacterSet currentGroup = new CharacterSet();
+            var groups = new List<Group>();
+            var currentAnswers = new CharacterCountDictionary();
+            int numPeople = 0;
+
             using (TextReader reader = File.OpenText("input.txt"))
             {
                 Action recordGroup = () =>
                 {
-                    answerGroups.Add(currentGroup);
-                    currentGroup = new CharacterSet();
+                    if (numPeople > 0)
+                    {
+                        groups.Add(new Group() { Answers = currentAnswers, NumPeople = numPeople });
+                        currentAnswers = new CharacterCountDictionary();
+                        numPeople = 0;
+                    }
                 };
 
                 while (reader.Peek() >= 0)
@@ -29,21 +42,34 @@ namespace Day6
                     }
                     else
                     {
+                        ++numPeople;
                         foreach (char character in line)
                         {
-                            currentGroup.Add(character);
+                            int count;
+                            if (currentAnswers.TryGetValue(character, out count))
+                            {
+                                ++count;
+                            }
+                            else
+                            {
+                                count = 1;
+                            }
+                            currentAnswers[character] = count;
                         }
                     }
                 }
 
                 recordGroup();
 
-                int sum = 0;
-                foreach (CharacterSet group in answerGroups)
+                int uniqueSum = 0;
+                int unifiedSum = 0;
+                foreach (var group in groups)
                 {
-                    sum += group.Count;
+                    uniqueSum += group.Answers.Count;
+                    group.Answers.Values.ToList().ForEach(answerCount => unifiedSum += (group.NumPeople == answerCount) ? 1 : 0);
                 }
-                Console.WriteLine("Sum: {0}", sum );
+                Console.WriteLine("Sum of unique answers per-group: {0}", uniqueSum);
+                Console.WriteLine("Sum of unified answers per-group: {0}", unifiedSum);
             }
         }
     }

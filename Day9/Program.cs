2b2fe2ff5e7102f4fead0bf38c1ba2d2c028d9cb
Stuart Brown <stbr@microsoft.com>
2020-12-09 13:09:15 +0000

    Day 9 part 2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -8,10 +8,10 @@ namespace Day9
     {
         static void Main(string[] args)
         {
+            const int preambleSize = 25;
             Int64[] numbers = Array.ConvertAll(File.ReadAllLines("input.txt"), item => Int64.Parse(item));
 
-            int preambleSize = 25;
-
+            Int64 badNumber = -1;
             for (int currentIndex = preambleSize; currentIndex < numbers.Length; ++currentIndex)
             {
                 var preamble = new ArraySegment<Int64>(numbers, currentIndex - preambleSize, preambleSize);
@@ -20,7 +20,26 @@ namespace Day9
                 // Part 1
                 if (joinedResults.Contains(numbers[currentIndex]) == false)
                 {
+                    badNumber = numbers[currentIndex];
                     Console.WriteLine("Invalid number: {0}", numbers[currentIndex]);
+                    break;
+                }
+            }
+
+            // Part 2
+            for (int currentIndex = 0; currentIndex < numbers.Length; ++currentIndex)
+            {
+                Int64 total = numbers[currentIndex];
+                for (int advanceIndex = currentIndex + 1; advanceIndex < numbers.Length; ++advanceIndex)
+                {
+                    total += numbers[advanceIndex];
+                    if (total == badNumber)
+                    {
+                        var smallest = new ArraySegment<Int64>(numbers, currentIndex, advanceIndex - currentIndex).ToList().Min();
+                        var largest = new ArraySegment<Int64>(numbers, currentIndex, advanceIndex - currentIndex).ToList().Max();
+                        Console.WriteLine("Found range {0}+{1} = {2}", smallest, largest, smallest + largest);
+                        break;
+                    }
                 }
             }
         }

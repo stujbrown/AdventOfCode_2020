e9ada846ed16a099e0ebdd9e07b490a6210eea15
stuart.j.brown.90@gmail.com <stuart.j.brown.90@gmail.com>
2020-12-15 20:15:07 +0000

    Day 15 part 2

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -9,21 +9,20 @@ namespace Day15
     {
         static void PlayGame(Int64[] seedNumbers, int numTurns)
         {
-            var spoken = new List<Int64>();
-            spoken.AddRange(seedNumbers);
+            var spoken = new Dictionary<Int64, Int64>();
+            for (int i = 0; i < seedNumbers.Length; ++i)
+                spoken.Add(seedNumbers[i], i);
 
             Int64 last = seedNumbers.Last();
-            for (int count = spoken.Count; count < numTurns; ++count)
+            for (int count = seedNumbers.Length; count < numTurns; ++count)
             {
                 Int64 num = 0;
-                int idx = spoken.LastIndexOf(last, spoken.Count - 2);
-                if (idx != -1)
-                {
-                    num = spoken.Count - 1 - idx;
-                }
+                Int64 turnCount;
+                if (spoken.TryGetValue(last, out turnCount))
+                    num = count - 1 - turnCount;
 
+                spoken[last] = count - 1;
                 last = num;
-                spoken.Add(last);
             }
 
             Console.WriteLine("Last spoken in {0} turn game: {1}", numTurns, last);
@@ -33,7 +32,7 @@ namespace Day15
         {
             Int64[] numbers = Array.ConvertAll(File.ReadAllLines("input.txt")[0].Split(","), item => Int64.Parse(item)).ToArray();
             PlayGame(numbers, 2020);
-           // PlayGame(numbers, 30000000);
+            PlayGame(numbers, 30000000);
         }
     }
 }

6894541daf3f7473544424809e8da5658eb03323
stuart.j.brown.90@gmail.com <stuart.j.brown.90@gmail.com>
2020-12-25 10:55:25 +0000

    Day 25 both parts

---
+++ Day25/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] publicKeys = Array.ConvertAll(File.ReadAllLines("input.txt"), line => long.Parse(line));
+
+            int testSubject = 7;
+            var keys = new long[2] { 1, 1 };
+            var loopCounts = new int[2] { 0, 0 };
+            for (int keyIndex = 0; keyIndex < 2; ++keyIndex)
+            {
+                while (keys[keyIndex] != publicKeys[keyIndex])
+                {
+                    keys[keyIndex] = (keys[keyIndex] * testSubject) % 20201227L;
+                    ++loopCounts[keyIndex];
+                }
+            }
+
+            long encryptionKey = 1;
+            Array.ForEach(Enumerable.Range(0, loopCounts[0]).ToArray(), i => encryptionKey = (encryptionKey * keys[1]) % 20201227L);
+            Console.WriteLine("Encryption key: {0}", encryptionKey);
+        }
+    }
+}
